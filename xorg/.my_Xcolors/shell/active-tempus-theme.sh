9cc68dea7bea712df0116ac45655f4a1c31a6f59
Protesilaos Stavrou <info@protesilaos.com>
2018-02-24 16:51:43 +0200

    Add wrappers for dmenu* that use Xcolors as shell variables

---
+++ xorg/.my_Xcolors/shell/active-tempus-theme.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Define the path to this file, so as not to change the reference to the active colour scheme.
+# This file is referenced by my own dmenu* wrappers.
+# See https://github.com/protesilaos/dotfiles, especially `bin`
+
+# source "$HOME/.my_Xcolors/shell/tempus-autumn.sh"
+# source "$HOME/.my_Xcolors/shell/tempus-fugit.sh"
+# source "$HOME/.my_Xcolors/shell/tempus-future.sh"
+# source "$HOME/.my_Xcolors/shell/tempus-past.sh"
+source "$HOME/.my_Xcolors/shell/tempus-rift.sh"
+# source "$HOME/.my_Xcolors/shell/tempus-spring.sh"
+# source "$HOME/.my_Xcolors/shell/tempus-summer.sh"
+# source "$HOME/.my_Xcolors/shell/tempus-totus.sh"
+# source "$HOME/.my_Xcolors/shell/tempus-warp.sh"
+# source "$HOME/.my_Xcolors/shell/tempus-winter.sh"
