32d256d4bca9931a7f3871b61fa76839c0b50dae
Protesilaos Stavrou <info@protesilaos.com>
2018-04-03 23:43:35 +0300

    Revert to my Emacs with some refined configs

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -27,3 +27,17 @@
 
 (provide 'init)
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (which-key wc-mode volatile-highlights visual-regexp uuidgen use-package unfill toc-org smeargle restart-emacs rainbow-delimiters popwin pcre2el orgit org-projectile org-present org-pomodoro org-mime org-download org-bullets open-junk-file neotree mwim multiple-cursors move-text mmm-mode markdown-toc magit-gitflow macrostep lorem-ipsum linum-relative link-hint indent-guide hungry-delete htmlize hl-todo highlight-parentheses highlight-numbers highlight-indentation gnuplot gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter gh-md fuzzy focus flycheck-pos-tip flx-ido fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell eval-sexp-fu elisp-slime-nav dumb-jump diminish define-word dashboard counsel column-enforce-mode auto-highlight-symbol auto-dictionary auto-compile aggressive-indent adaptive-wrap ac-ispell))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
