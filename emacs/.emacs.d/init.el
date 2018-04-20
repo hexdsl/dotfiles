86c0863f6ae5e1be4ca69593666e4ddd98b50731
Protesilaos Stavrou <info@protesilaos.com>
2018-04-20 23:03:52 +0300

    Continue adapting dotfiles to Debian base.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -9,6 +9,28 @@
 ;; the idea is to see whether Emacs can offer more
 
 ;;; Code:
+
+;;;; custom
+;;;; NOTE this should come first to avoid prompts on startup
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("58587075e282dcb38fc873d2f6e0d71e52872f214bc1c632f5b48dc130d6e12a" default)))
+ '(package-selected-packages
+   (quote
+    (which-key wc-mode volatile-highlights visual-regexp uuidgen use-package unfill toc-org smeargle restart-emacs rainbow-delimiters popwin pcre2el orgit org-projectile org-present org-pomodoro org-mime org-download org-bullets open-junk-file neotree mwim multiple-cursors move-text mmm-mode markdown-toc magit-gitflow macrostep lorem-ipsum linum-relative link-hint indent-guide hungry-delete htmlize hl-todo highlight-parentheses highlight-numbers highlight-indentation gnuplot gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter gh-md fuzzy focus flycheck-pos-tip flx-ido fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell eval-sexp-fu elisp-slime-nav dumb-jump diminish define-word dashboard counsel column-enforce-mode auto-highlight-symbol auto-dictionary auto-compile aggressive-indent adaptive-wrap ac-ispell))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
 ;;;; packages
 (require 'package)
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
@@ -27,20 +49,3 @@
 
 (provide 'init)
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("58587075e282dcb38fc873d2f6e0d71e52872f214bc1c632f5b48dc130d6e12a" default)))
- '(package-selected-packages
-   (quote
-    (which-key wc-mode volatile-highlights visual-regexp uuidgen use-package unfill toc-org smeargle restart-emacs rainbow-delimiters popwin pcre2el orgit org-projectile org-present org-pomodoro org-mime org-download org-bullets open-junk-file neotree mwim multiple-cursors move-text mmm-mode markdown-toc magit-gitflow macrostep lorem-ipsum linum-relative link-hint indent-guide hungry-delete htmlize hl-todo highlight-parentheses highlight-numbers highlight-indentation gnuplot gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter gh-md fuzzy focus flycheck-pos-tip flx-ido fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell eval-sexp-fu elisp-slime-nav dumb-jump diminish define-word dashboard counsel column-enforce-mode auto-highlight-symbol auto-dictionary auto-compile aggressive-indent adaptive-wrap ac-ispell))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
