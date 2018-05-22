30304533b4308a384fadb2f9da8121c20435015b
Protesilaos Stavrou <info@protesilaos.com>
2018-05-22 12:12:47 +0300

    Abstracted environment font for scripts. Updated to Roboto.

---
+++ shell/.my_custom_ui_font.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This is sourced from some of my scripts at ~/bin
+# The idea is to define the custom font once
+
+# PREREQUITE https://github.com/protesilaos/dotfiles
+
+# used with dmenu scripts
+my_custom_ui_font='Roboto-14:Regular'
+
+# TODO add more abstract variables with simpler font declaration syntax
